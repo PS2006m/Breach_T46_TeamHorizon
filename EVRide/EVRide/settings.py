94d43530d20b55a1c2938ba82adc27d3f2756647
Pratham Shah <154831899+PS2006m@users.noreply.github.com>
2025-03-23 08:32:11 +0530

    settings.py

--- EVRide/EVRide/settings.py
+++ EVRide/EVRide/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c6#q4j488aomj33y28uw#jkpwk_2uoevv@3#en#%yvl+w^*vae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
